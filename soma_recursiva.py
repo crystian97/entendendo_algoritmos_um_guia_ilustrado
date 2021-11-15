6d286d87b1806a734a76415e387d3b17aeb98221
crystian97 <icrystianveloso@gmail.com>
2021-11-15 15:02:47 -0300

    soma recursiva

---
+++ soma_recursiva.py
@@ -0,0 +1,11 @@
+def soma(array):
+    if len(array) == 1:
+        return array[0]
+    elif len(array)== 0:
+        return 0
+    else:
+        return array[0]  + soma(array[1:]) ##1: significa que vai de 0 até o penultimo elemento
+
+numerosPrimos =[1,2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97]
+n=[]
+print(soma(n))
