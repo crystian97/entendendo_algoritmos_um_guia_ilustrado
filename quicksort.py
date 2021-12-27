00597ae531682b4f8195f6b693a06dee6d6851f9
crystian97 <icrystianveloso@gmail.com>
2021-12-27 11:31:00 -0300

    qsort

--- quicksort.py
+++ quicksort.py
@@ -1,14 +1,21 @@
+import random
+from random import shuffle
 # algoritmo quicksort
-def quick_sort(lista):
-    if len(lista) <= 1:
+def qsort(lista):
+    # verifica se a lista é de um unico elemento ou vazia
+    if len(lista) <2:
         return lista
-    pivo = lista[0]
-    # aqui pegamos o primeiro elemento da lista e colocamos em um pivo
-    # depois dividimos a lista em duas partes, uma com os elementos menores que o pivo e outra com os maiores
-    # e ao final retornamos a junção das duas partes
-    menores = [x for x in lista[1:] if x <= pivo]# [1:]] percorrer do primeiro até o ultimo elemento do array
-    maiores = [x for x in lista[1:] if x > pivo]
-    return quick_sort(menores) + [pivo] + quick_sort(maiores)
-primos_ate_50 =[1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
-print(quick_sort(primos_ate_50))
+    else:
+        # escolhemos o elemento pivo para se basear a ordenação de forma aleatória
+        pivo = random.choice(lista);
+        # separar a lista em duas , uma contando os elemtentos menores que o pivo e outra com os maiores
+        menores = [ i for i in lista[1:] if i <= pivo]
+        maiores = [i for i in lista[1:] if i > pivo]
+    # aqui retornamos o array ordenado
+    return qsort(menores) + [pivo] + qsort(maiores)
+numeros_aleatorios_ate_50 = [ i for i in range(50)]
+# bagunçando os numeros
+shuffle(numeros_aleatorios_ate_50)
 
+print(numeros_aleatorios_ate_50)
+print(qsort(numeros_aleatorios_ate_50))
